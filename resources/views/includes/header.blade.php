9b8dbfbcfea154d25fec97a778519ca08d132e6b
Satria Ananta <satriaanantagaming@gmail.com>
2020-11-28 12:25:38 +0800

    update

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <a class="navbar-brand" href="#">
         <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-            Maju Mapan Klinik
+            Smart Clinic
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
