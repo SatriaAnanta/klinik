47f31e9a7bad077453cd64189c2bc1e57fa4f5f3
Satria Ananta <satriaanantagaming@gmail.com>
2020-11-28 12:04:03 +0800

    add new file

---
+++ resources/views/template.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('includes.head')
+    </head>
+    <body>
+        <header>
+            @include('includes.header')
+        </header>
+        Isi Konten Disini
+        <footer class="footer">
+            @include('includes.footer')
+        </footer>
+    </body>
+<html>
