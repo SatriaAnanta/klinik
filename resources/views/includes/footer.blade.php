1b60e9c8bf0b8ac58cc2784f2b4748695fdbfa57
Satria Ananta <satriaanantagaming@gmail.com>
2020-11-28 12:22:19 +0800

    update

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,21 @@
 <div class="container">
     <div class="footer-copyright">
         <div class="footer-copyright-text-left">
-            Mbuh
+            &copy {{ date('Y') }} Smart Clinic All Rights Reserved
         </div>
         <div class="footer-copyright-link-right">
-            <a href="google.com">Google</a>
+        <a href="https://www.facebook.com" style="padding: 0.5rem">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://www.google.com" style="padding: 0.5rem">
+            <i class="fab fa-google"></i>
+        </a>
+        <a href="https://www.twitter.com" style="padding: 0.5rem">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.instagram.com" style="padding: 0.5rem">
+            <i class="fab fa-instagram"></i>
+        </a>
         </div>
     </div>
 </div>
