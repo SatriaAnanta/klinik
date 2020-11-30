17f9ce60f9e5baec49ade3d140ad8f4ec549ac2b
Satria Ananta <satriaanantagaming@gmail.com>
2020-11-30 17:17:40 +0800

    update

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -26,7 +26,7 @@
         </nav>
         <div class="container">
             <div class="row">
-                <div class="col-md-6 ml-auto mr-auto">
+                <div class="col-md-6 ml-auto mr-auto mb-4">
                     <div class="profile">
                         <div class="avatar">
                             <img src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU0NjQzOTk4OTQ4OTkyMzQy/ansel-elgort-poses-for-a-portrait-during-the-baby-driver-premiere-2017-sxsw-conference-and-festivals-on-march-11-2017-in-austin-texas-photo-by-matt-winkelmeyer_getty-imagesfor-sxsw-square.jpg" alt="Circle Image" class="img-raised rounded-circle img-fluid">
@@ -38,9 +38,33 @@
                         <div class="description text-center">
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rem autem quaerat eaque nemo provident illo facere similique consectetur vel obcaecati ab impedit, cum quae, ea aliquam dignissimos architecto fugit!</p>
                         </div>
-                        <button type="button" role="button"  class="btn btn-primary"> BUAT JANJI </button>
+                        <button type="button" role="button"  class="btn btn-primary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> BUAT JANJI </button>
                     </div>
                 </div>
+
+                <!-- <div class="col-md-12 ml-auto mr-auto mb-4">
+                    <div class="collapse" id="collapseExample">
+                        <div class="card card-body">
+                            <div class="d-flex flex-column flex-md-row align-items-start align-items-md-center border-bottom mb-4">
+                                <div class="d-flex flex-md-column align-items-center">
+                                    Pagi
+                                </div>
+                                <div class="btn-group-horizontal" data-toggle="buttons" id ="Group">
+                                    <label class="btn btn-sm btn-info btnSelect active" **id="Option 1"** style="margin-left: 18px;">
+                                        <input type="radio" name="options" id="Option 1" autocomplete="off" checked> 07.30
+                                    </label>
+                                    <label class="btn btn-sm btn-info btnSelect" **id="Option 2"** style="margin-left: 18px;">
+                                        <input type="radio" name="options" id="Option 2" autocomplete="off"> 09.00
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="btn-group-horizontal text-center">
+                                <button type="button" role="button" class="btn btn-primary" style="margin-left: 18px;"> Batal </button>
+                                <button type="button" role="button" class="btn btn-primary" style="margin-left: 18px;"> Lanjutkan </button>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
             </div>
         </div>
         </section>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/material', function () {
+Route::get('/a', function () {
     return view('welcome');
 });
 
