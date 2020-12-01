140112554625d303c9b7dec08a262723620016a5
denlulung <denlulung@avo.co.id>
2020-12-01 17:57:18 +0700

    add seeder doctor

--- database/seeders/DoctorsSeeder.php
+++ database/seeders/DoctorsSeeder.php
@@ -49,5 +49,29 @@ class DoctorsSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+        
+        DB::table('doctors')->insert([
+            'name' => 'dr. Ina',
+            'bio' => 'Dokter ina merupakan tenaga medis junior',
+            'img' => 'https://image.freepik.com/free-photo/joyful-young-female-doctor-medical-robe-with-stethoscope-puts-hands-together-chin-looks-side-isolated-orange-background-with-copy-space_141793-34476.jpg',
+            'slug' => 'dr-ina',
+            'experience' => 1,
+            'specialty_id' => 1,
+            'cost' => 1500,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('doctors')->insert([
+            'name' => 'dr. Dimas',
+            'bio' => 'Dokter Asep merupakan tenaga medis penanganan covid',
+            'img' => 'tes.jpg',
+            'slug' => 'dr-dimas',
+            'experience' => 1,
+            'specialty_id' => 1,
+            'cost' => 1500,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
